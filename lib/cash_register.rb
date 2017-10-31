74acf052a724ec4da130417b0316d9c1e9079ba0
Alyssa Watson <alyssa.s.watson@gmail.com>
2017-10-31 03:09:40 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,16 @@
 
+class CashRegister
+  attr_accessor :total, :discount
+
+  def initialize(discount = nil)
+    @total = 0
+    @discount = discount
+  end
+
+  def add_item(title, price)
+    self.total += price
+
+  end
+
+
+end
